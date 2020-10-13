8378cb2549c07dcb87c3b808504c6314c96ed1e6
pie <returnpie@icloud.com>
2020-10-14 02:53:58 +0900

    set urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from blog.views import post_publish
 from django.conf.urls import url
 from blog import views
 
@@ -11,5 +12,11 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/remove/$',
         views.PostDeleteView.as_view(), name='post_remove'),
     url(r'^drafts/$', views.DraftListView.as_view(), name='post_draft_list'),
-
+    url(r'^post/(?P<pk>\d+)/comment/$',
+        views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/approve/$',
+        views.comment_approve, name='comment_approve'),
+    url(r'^comment/(?P<pk>\d+)/remove/$',
+        views.comment_remove, name='comment_remove'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish')
 ]
